939e4588869cf97dde2463abea6e82edf37386f2
balkrushna2830 <balkrushnamore@outlook.com>
2020-08-24 16:43:13 +0530

    Updates

---
+++ precache-manifest.be11132626d3a7d1f560063c01ea76e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47b17afd8410e304dda81b07e56d3488",
+    "url": "/react-cartoon/index.html"
+  },
+  {
+    "revision": "971ef2721caf9d94be1f",
+    "url": "/react-cartoon/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e3c9f4f5eb3cecd92ea6",
+    "url": "/react-cartoon/static/css/main.7c55dcad.chunk.css"
+  },
+  {
+    "revision": "971ef2721caf9d94be1f",
+    "url": "/react-cartoon/static/js/2.c0d4bb0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-cartoon/static/js/2.c0d4bb0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3c9f4f5eb3cecd92ea6",
+    "url": "/react-cartoon/static/js/main.9705add5.chunk.js"
+  },
+  {
+    "revision": "dda23218d86131781999",
+    "url": "/react-cartoon/static/js/runtime-main.e7750283.js"
+  },
+  {
+    "revision": "43200e891da47c2987dad36141410f3e",
+    "url": "/react-cartoon/static/media/dor.43200e89.png"
+  }
+]);
